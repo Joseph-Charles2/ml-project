ec33d9d6b87b72be26a56f52ac03b94da4fa4bb2
Joseph charles <josephcharles.19988@gmail.com>
2025-01-15 13:03:21 +0530

    renamed expection to exception.py

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,4 @@
 import sys
-import logging
 
 
 def error_message_detail(error,error_detail:sys):
@@ -16,8 +15,11 @@ class CustomException(Exception):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message,error_detail=error_detail)
 
-
     def __str__(self):
+
         return self.error_message
+
+
+
 
 
